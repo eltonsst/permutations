c96ff209d57a27a8d408139c1c4b3c4ad412a0a7
Elton Stafa <61477846+eltonsst@users.noreply.github.com>
2021-05-25 20:25:41 +0200

    Create Main.hs

---
+++ Main.hs
@@ -0,0 +1,21 @@
+module Main where
+  
+import Data.List
+
+main = print (permutations [1 .. 12])
+
+
+{--
+defined in the standard library: 
+
+
+permutations            :: [a] -> [[a]]
+permutations xs0        =  xs0 : perms xs0 []
+  where
+    perms []     _  = []
+    perms (t:ts) is = foldr interleave (perms ts (t:is)) (permutations is)
+      where interleave    xs     r = let (_,zs) = interleave' id xs r in zs
+            interleave' _ []     r = (ts, r)
+            interleave' f (y:ys) r = let (us,zs) = interleave' (f . (y:)) ys r
+                                     in  (y:us, f (t:y:us) : zs)
+--}
